19180decbc9533101945335ba441c0463a7de976
Shelly Seroussi <shellyser@gmail.com>
2015-03-20 03:39:12 -0400

    Changed db to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150319024751) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "athletes", force: true do |t|
     t.string   "firstname"
     t.string   "lastname"
